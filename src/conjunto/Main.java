1a807c15837f5cbf368dea6f2674577e0e8175b6
nmontagut99 <nmontagut99@gmail.com>
2023-04-06 20:55:52 -0500

    bases para iniciar el proyecto

---
+++ src/conjunto/Main.java
@@ -0,0 +1,23 @@
+package conjunto;
+
+import java.util.Scanner;
+
+public class Main {
+	static boolean bandera;
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		// REPETIR EL PROYECTO
+		do {
+			System.out.println("Conjunto residencial chupamEstePenco");
+			System.out.println("------------------------------------");
+			
+			
+			
+			
+			
+			
+			System.out.println("Desea continuar?");
+			bandera = entrada.nextBoolean();
+		}while(bandera);
+	}
+}
---
+++ src/entidades/Persona.java
@@ -0,0 +1,54 @@
+package entidades;
+
+public class Persona {
+	// ATRIBUTOS
+	String nombre;
+	int telefono;
+	byte edad;
+	int cedula;
+	String genero;
+	Boolean Colombiano;
+	// CONSTRUCTOR VACIO
+	public Persona() {
+		// VACIO
+	}
+	// METODOS
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public byte getEdad() {
+		return edad;
+	}
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+	public int getCedula() {
+		return cedula;
+	}
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public Boolean getColombiano() {
+		return Colombiano;
+	}
+	public void setColombiano(Boolean colombiano) {
+		Colombiano = colombiano;
+	}
+	
+	
+}
